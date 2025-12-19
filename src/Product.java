ff73b63303defc4402feef8667d900f0041687ab
rustam <rustiktumurzaev@gmail.com>
2025-12-20 02:10:40 +0500

    Week2

---
+++ src/Product.java
@@ -0,0 +1,24 @@
+package rustam;
+
+public class Product {
+    public int id, quantity;
+    public String name;
+    public double price;
+
+
+    public String getInfo() {
+        return "Id: " + id +
+                " Name: " + name +
+                " Price: " + price +
+                " Quantity: " + quantity ;
+    }
+
+    public boolean updateQuantity(int amount) {
+        if (amount <= quantity) {
+            quantity -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
